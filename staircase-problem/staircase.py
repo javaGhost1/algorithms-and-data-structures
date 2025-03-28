1f26828d186e30e93280390be71e821e4bbb1b2f
javaGhost1 <dennismurage97@gmail.com>
2025-03-28 18:39:33 +0300

    added more functions to handle different logic

--- staircase-problem/staircase.py
+++ staircase-problem/staircase.py
@@ -11,9 +11,21 @@ def staircase(n):
         #add spaces and #
         print(' '*(n-i)+'#'*i)
 
+def leftStaircase(n):
+    """Start from right"""
 
+    for i in range(1, n+1):
+        print('#'*i+' '*(n-i))
+
+def doubleStaircase(n):
+    """Print double staircase"""
+
+    for i in range(1, n+1):
+        print(' '*(n-i)+'#'*i+'#'*i+' '*(n-i))
 
 if __name__ == '__main__':
     print("Choose the number of stairs you want: ")
     n = int(input().strip())
+    leftStaircase(n)
     staircase(n)
+    doubleStaircase(n)
