4e7dc40e3567ab93157d48136dfa9f4b10b5641e
javaGhost1 <dennismurage97@gmail.com>
2025-04-06 05:48:54 +0300

    first attempt to solve character replcement problem

--- longest-repeats-replacement/lr_string.py
+++ longest-repeats-replacement/lr_string.py
@@ -10,13 +10,18 @@ def character_replacement(s: str, k: int):
     max_len=0
     # track using right
     for right in range(len(s)):
-        # update character count
-        char_map[s[right]] = 
+        # update character count use get
+        char_map[s[right]] = char_map.get[s[right], 0]+1
         # update max_freq
-
-
-
-
+        max_freq = max(max_freq, char_map[s[right]])
+        # Check if we need more than k replacements
+        if (right-left+1) - max_freq > k:
+            # Remove the leftmost character from the window
+            char_map[s[left]] -= 1
+            left += 1 # Shrink the window
+        # update mx length
+        max_len = max(max_len, right-left+1)
+    return max_len
 
 
 s = "AABABBA"
